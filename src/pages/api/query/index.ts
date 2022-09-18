223a1538dc6b23e84453a68ca6392f8f412e3af2
pheralb <pablohdez4340@gmail.com>
2022-09-18 19:21:26 +0100

    ⚒️ API & middleware config.

---
+++ src/pages/api/query/index.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/server/db/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    return await createNewLink(req, res);
+  } else {
+    return res
+      .status(405)
+      .json({ message: "[X] Method not allowed: ", success: false });
+  }
+}
+
+async function createNewLink(req: NextApiRequest, res: NextApiResponse) {
+  const body = req.body;
+  try {
+    const newLink = await prisma.link.create({
+      data: {
+        slug: body.slug,
+        url: body.url,
+        description: body.description,
+      },
+    });
+    return res.status(200).json(newLink);
+  } catch (error) {
+    console.error("[X] Request error:", error);
+    res.status(500).json({ error: "[X] Error creating question:", success: false });
+  }
+}
---
+++ src/pages/api/url/[slug].ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/server/db/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { slug } = req.query;
+
+  if (!slug || typeof slug !== "string") {
+    return res.status(400).json({
+      error: "[X] Error: Missing slug? Remember that urls start like this: /u/yourLink",
+    });
+  }
+
+  const data = await prisma.link.findFirst({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+
+  if (!data) {
+    return res.status(404).json({
+      error: "[X] Error: Link not found or removed. Go to urll.vercel.app and create a new link.",
+    });
+  }
+
+  // Cache:
+  res.setHeader("Cache-Control", "s-maxage=1000000, stale-while-revalidate");
+
+  return res.json(data);
+};
