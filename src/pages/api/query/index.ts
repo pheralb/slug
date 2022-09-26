70286b6475566ca9454ad2a3fcb81b72cb25838b
pheralb <pablohdez4340@gmail.com>
2022-09-26 17:16:03 +0100

    ⚒️ Protect createNewLink with getServerAuthSession.

--- src/pages/api/query/index.ts
+++ src/pages/api/query/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/server/db/client";
+import { getServerAuthSession } from "@/server/common/get-server-auth-session";
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,21 +17,28 @@ export default async function handler(
 
 // Create new link:
 async function createNewLink(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getServerAuthSession({ req, res });
   const body = req.body;
-  try {
-    const newLink = await prisma.link?.create({
-      data: {
-        slug: body.slug,
-        url: body.url,
-        description: body.description,
-        creatorId: body.creatorId,
-      },
-    });
-    return res.status(200).json(newLink);
-  } catch (error) {
-    console.error("[X] Request error:", error);
-    res
-      .status(500)
-      .json({ error: "[X] Error creating question:", success: false });
+  if (session) {
+    try {
+      const newLink = await prisma.link?.create({
+        data: {
+          slug: body.slug,
+          url: body.url,
+          description: body.description,
+          creatorId: body.creatorId,
+        },
+      });
+      return res.status(200).json(newLink);
+    } catch (error) {
+      console.error("[X] Request error:", error);
+      res
+        .status(500)
+        .json({ error: "[X] Error creating question:", success: false });
+    }
+  } else {
+    return res
+      .status(401)
+      .json({ error: "[X] Error: Not logged in.", success: false });
   }
 }
