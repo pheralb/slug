270bdf72ba7ddebb5e2bb0b56d4d0d41095d4191
pheralb <pablohdez4340@gmail.com>
2022-09-28 00:27:14 +0100

    ⚒️ Create card component.

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/styles/ui";
 import React from "react";
-import { BiLinkAlt, BiLinkExternal } from "react-icons/bi";
+import toast from "react-hot-toast";
+import { BiCopy, BiLinkAlt, BiLinkExternal } from "react-icons/bi";
 
 interface CardProps {
   url: string;
@@ -9,20 +11,46 @@ interface CardProps {
 }
 
 const Card = (props: CardProps) => {
+  const copyToClipboard = async (txt: string) => {
+    try {
+      const clipboardItem = new ClipboardItem({
+        "text/plain": new Blob([txt], { type: "text/plain" }),
+      });
+      await navigator.clipboard.write([clipboardItem]);
+    } catch (error) {
+      await navigator.clipboard.writeText(txt);
+    }
+    toast("Copied to clipboard", {
+      icon: "🚀",
+      style: {
+        borderRadius: "10px",
+        background: "#28283E",
+        color: "#fff",
+      },
+    });
+  };
+
   return (
     <div
       className={`bg-midnightLight rounded-lg shadow-sm p-4 ${props.className}`}
     >
-      <a
-        className="text-gray-200 text-xl"
-        href={`https://slug.vercel.app/s/${props.slug}`}
-      >
-        /s/{props.slug}
-      </a>
-      <div className="flex items-center mb-3 text-gray-400">
-        <BiLinkAlt />
-        <p className="truncate ml-1">{props.url}</p>
+      <div className="flex items-center">
+        <a
+          className="text-gray-200 text-xl"
+          href={`https://slug.vercel.app/s/${props.slug}`}
+        >
+          /s/{props.slug}
+        </a>
+        <button
+          className="p-1 ml-1 text-gray-500 hover:text-gray-200 transition-colors duration-200"
+          onClick={() =>
+            copyToClipboard(`https://slug.vercel.app/s/${props.slug}`)
+          }
+        >
+          <BiCopy className="mr-2" />
+        </button>
       </div>
+      <p className="truncate text-gray-400 mb-2">{props.url}</p>
       <p className="text-gray-400">{props.description}</p>
     </div>
   );
