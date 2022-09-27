7a9ca04b102c14774c64103c431a098c77bc8760
pheralb <pablohdez4340@gmail.com>
2022-09-28 00:27:52 +0100

    ⚒️ Create form improvements.

--- src/components/create/index.tsx
+++ src/components/create/index.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
-
 import { trpc } from "@/utils/trpc";
 import { CreateLinkInput } from "@/schema/link.schema";
-
-import { Button, Input, Textarea } from "@/styles/ui";
+import { Button } from "@/styles/ui";
 import { BiRocket } from "react-icons/bi";
 import Loader from "@/motions/loader";
-
+import Messages from "../messages";
+import { nanoid } from "nanoid";
+import toast from "react-hot-toast";
 interface linkData {
   id: string;
   url: string;
@@ -17,7 +17,12 @@ interface linkData {
 }
 
 const Create = () => {
-  const { handleSubmit, register } = useForm<CreateLinkInput>();
+  const {
+    handleSubmit,
+    register,
+    setValue,
+    formState: { errors },
+  } = useForm<CreateLinkInput>();
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
@@ -25,6 +30,14 @@ const Create = () => {
     onSuccess: () => {
       router.push(`/dash`);
       setLoading(false);
+      toast("Link created successfully", {
+        icon: "🥳",
+        style: {
+          borderRadius: "10px",
+          background: "#28283E",
+          color: "#fff",
+        },
+      });
     },
     onError: () => {
       setLoading(false);
@@ -36,30 +49,62 @@ const Create = () => {
     mutate(values);
   };
 
+  const handleGenerateRandomSlug = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const randomSlug = nanoid(6);
+    setValue("slug", randomSlug);
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {error && <p>{JSON.stringify(error)}</p>}
+      {error && (
+        <div
+          className="flex border border-red-700 rounded-lg p-4 mb-5 text-sm text-red-500"
+          role="alert"
+        >
+          <p>
+            <span className="font-medium">Danger alert!</span> Change a few
+            things up and try submitting again.
+          </p>
+        </div>
+      )}
       <div className="mb-5">
         <label htmlFor="url">Enter the URL here:</label>
         <input
           id="url"
           type="text"
-          required
           placeholder="https://"
           className="rounded-md px-4 py-2 w-full focus:border-none mt-1 bg-midnightLight text-white"
-          {...register("url")}
+          {...register("url", {
+            required: {
+              value: true,
+              message: "Please enter a URL.",
+            },
+          })}
         />
+        {errors.url && <Messages text={errors.url.message} className="mt-2" />}
       </div>
       <div className="mb-5">
         <label htmlFor="slug">Custom slug:</label>
-        <input
-          id="slug"
-          type="text"
-          required
-          placeholder="Custom slug"
-          className="rounded-md px-4 py-2 w-full focus:border-none mt-1 bg-midnightLight text-white"
-          {...register("slug")}
-        />
+        <p className="text-gray-500">https://slug.vercel.app/s/</p>
+        <div className="flex items-center justify-between mt-1">
+          <input
+            id="slug"
+            type="text"
+            placeholder="Custom slug"
+            className="rounded-md px-4 py-2 w-full focus:border-none bg-midnightLight text-white"
+            {...register("slug", {
+              required: {
+                value: true,
+                message: "Please enter a custom slug or generate a random.",
+              },
+            })}
+          />
+          <Button onClick={handleGenerateRandomSlug} className="ml-2 bg-midnightLight">Random</Button>
+        </div>
+        {errors.slug && (
+          <Messages text={errors.slug.message} className="mt-2" />
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="description">Description (optional):</label>
