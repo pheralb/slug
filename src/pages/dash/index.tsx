df72c93770b477c30cc09ad00263d32dbe42cc6b
pheralb <pablohdez4340@gmail.com>
2022-09-28 22:21:18 +0100

    ⚒️ Fix view cards.

--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -5,6 +5,7 @@ import Loader from "@/motions/loader";
 
 import Card from "@/components/card";
 import DashboardLayout from "@/layout/dashboard";
+import Messages from "@/components/messages";
 
 const Dashboard = () => {
   const { data, error, isLoading } = trpc.useQuery(["links.links"]);
@@ -13,21 +14,24 @@ const Dashboard = () => {
     <DashboardLayout>
       {isLoading && (
         <>
-          <div className="flex justify-center items-center mt-8">
+          <div className="flex flex-col justify-center items-center mt-8">
+            <p className="mb-2">Loading your links...</p>
             <Loader width={40} height={40} />
           </div>
         </>
       )}
-      {error && <p>{JSON.stringify(error)}</p>}
-      {data?.map((link) => (
-        <Card
-          key={link.id}
-          url={link.url}
-          slug={link.slug}
-          description={link.description || "No description"}
-          className="mt-3"
-        />
-      ))}
+      {error && <Messages text="An error has occurred obtaining the links." />}
+      {data
+        ?.sort((a, b) => b.id - a.id)
+        .map((link) => (
+          <Card
+            key={link.id}
+            url={link.url}
+            slug={link.slug}
+            description={link.description || "No description"}
+            className="mt-3"
+          />
+        ))}
     </DashboardLayout>
   );
 };
