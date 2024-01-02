ed79055cb021587a4b2d560d2127bee68aecfbb6
pheralb <pablohdez4340@gmail.com>
2024-01-02 11:58:35 +0000

    Add github & google providers.

--- auth.config.ts
+++ auth.config.ts
@@ -2,27 +2,42 @@ import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 
+// Server utils & schemas:
 import { loginSchema } from "@/server/schemas";
 import { getUserByEmail } from "@/server/utils/user";
 
+// Environment variables:
+import { env } from "@/env.mjs";
+
 // Providers:
-// import GitHub from "next-auth/providers/github";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export default {
   providers: [
+    Google({
+      clientId: env.GOOGLE_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = loginSchema.safeParse(credentials);
+
         if (validatedFields.success) {
           const { email, password } = validatedFields.data;
+
           const user = await getUserByEmail(email);
           if (!user?.password) return null;
-          const passwordMatch = await bcrypt.compare(
-            password,
-            user.password as string,
-          );
-          if (passwordMatch) return user;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
         }
+
         return null;
       },
     }),
