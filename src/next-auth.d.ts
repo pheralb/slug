084f74f450c7dd5d09bcdfafed611da1d6b44b98
pheralb <pablohdez4340@gmail.com>
2024-01-02 12:09:35 +0000

    Add user types.

---
+++ src/next-auth.d.ts
@@ -0,0 +1,14 @@
+import type { UserRole } from "@prisma/client";
+import { type DefaultSession } from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+  isTwoFactorEnabled: boolean;
+  isOAuth: boolean;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
