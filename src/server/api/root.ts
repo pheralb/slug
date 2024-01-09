83d4a83a40d4223971b4631257806df3a24a4841
Pablo Hdez <62877300+pheralb@users.noreply.github.com>
2024-01-09 13:25:37 +0000

    Delete trpc server.

--- src/server/api/root.ts
+++
@@ -1,8 +0,0 @@
-import { linkRouter } from "@/server/api/routers/links.router";
-import { createTRPCRouter } from "@/server/trpc";
-
-export const appRouter = createTRPCRouter({
-  linksRouter: linkRouter,
-});
-
-export type AppRouter = typeof appRouter;
