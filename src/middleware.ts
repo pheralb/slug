e1b145e18a51957b88ba0754902d5ab29b90aad9
pheralb <pablohdez4340@gmail.com>
2024-03-08 11:37:49 +0000

    Fixed too many redirects when the user is not authenticated

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,7 +13,6 @@ const { auth } = NextAuth(authConfig);
 
 const middleware = async (req: NextRequest, isLoggedIn: boolean) => {
   try {
-    
     const { nextUrl } = req;
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
@@ -27,17 +26,20 @@ const middleware = async (req: NextRequest, isLoggedIn: boolean) => {
     }
 
     // Is Auth Route. First, check is authenticated:
-    if (isAuthRoute && isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT_URL, nextUrl));
-    }
-
-    // Protected routes:
-    if (!isLoggedIn && !isPublicRoute) {
-      return Response.redirect(new URL("/login", nextUrl));
+    if (isAuthRoute) {
+      if (isLoggedIn) {
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT_URL, nextUrl));
+      }
+      return null;
     }
 
     // Check ``slug`` route.
-    if (!isDashboardRoute && !isAuthRoute && !isApiAuthRoute) {
+    if (
+      !isDashboardRoute &&
+      !isAuthRoute &&
+      !isApiAuthRoute &&
+      !isPublicRoute
+    ) {
       const data = await fetch(
         `${req.nextUrl.origin}/api/url?slug=${slugRoute}`,
       );
@@ -54,6 +56,18 @@ const middleware = async (req: NextRequest, isLoggedIn: boolean) => {
       }
     }
 
+    // Protected routes:
+    if (!isLoggedIn && !isPublicRoute) {
+      let callbackUrl = nextUrl.pathname;
+      if (nextUrl.search) {
+        callbackUrl += nextUrl.search;
+      }
+      const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+      return Response.redirect(
+        new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+      );
+    }
+
     return null;
   } catch (error) {
     console.error("Error in middleware:", error);
@@ -65,7 +79,7 @@ export default auth((req) => middleware(req, !!req.auth));
 
 export const config = {
   matcher: [
-    //"/((?!api/|_next/|_proxy/|_static|_vercel|[\\w-]+\\.\\w+).*)",
+    "/((?!api/|_next/|_proxy/|_static|_vercel|[\\w-]+\\.\\w+).*)",
     "/s/:slug*",
   ],
 };
