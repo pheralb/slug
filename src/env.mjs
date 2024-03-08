81fb89355a38f1b65ed7daf08460b759782fa8d5
pheralb <pablohdez4340@gmail.com>
2024-03-08 11:36:55 +0000

    Add max_urls env variable

--- src/env.mjs
+++ src/env.mjs
@@ -30,6 +30,9 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: z.string(),
     RESEND_API_KEY: z.string(),
   },
+  client: {
+    NEXT_PUBLIC_MAX_URLS_PER_USER: z.string().min(1),
+  },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
@@ -40,6 +43,7 @@ export const env = createEnv({
     GOOGLE_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
+    NEXT_PUBLIC_MAX_URLS_PER_USER: process.env.NEXT_PUBLIC_MAX_URLS_PER_USER,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
