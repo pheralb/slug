771f378435a6ae0fbfbcf5b830278f9759085e94
pheralb <pablohdez4340@gmail.com>
2024-03-10 12:50:45 +0000

    Update auth.config with new next-auth settings

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,17 @@
 import type { NextAuthConfig } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
+import NextAuth from "next-auth";
 
-// Server utils & schemas:
-import { loginSchema } from "@/server/schemas";
-import { getUserByEmail } from "@/server/utils/user";
+// import bcrypt from "bcryptjs";
+
+// Adapters:
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+// Server:
+// import { loginSchema } from "@/server/schemas";
+import { db } from "@/server/db";
+import { getAccountByUserId } from "@/server/utils/account";
+import { getUserById } from "@/server/utils/user";
+import { getTwoFactorConfirmationByUserId } from "@/server/utils/two-factor-confirm";
 
 // Environment variables:
 import { env } from "@/env.js";
@@ -13,7 +20,8 @@ import { env } from "@/env.js";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
-export default {
+const config = {
+  adapter: PrismaAdapter(db),
   providers: [
     Google({
       clientId: env.GOOGLE_ID,
@@ -23,23 +31,79 @@ export default {
       clientId: env.GITHUB_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
-    Credentials({
-      async authorize(credentials) {
-        const validatedFields = loginSchema.safeParse(credentials);
-
-        if (validatedFields.success) {
-          const { email, password } = validatedFields.data;
-
-          const user = await getUserByEmail(email);
-          if (!user?.password) return null;
-
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-
-          if (passwordsMatch) return user;
-        }
-
-        return null;
-      },
-    }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+    error: "/auth-error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification:
+      if (account?.provider !== "credentials") return true;
+
+      // Check if user exists:
+      const existingUser = await getUserById(user.id);
+
+      // Prevent sign in without email verification:
+      if (!existingUser?.emailVerified) return false;
+
+      // Prevent sign in with two factor enabled:
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id,
+        );
+
+        if (!twoFactorConfirmation) return false;
+
+        // Delete two factor confirmation for next sign in:
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        });
+      }
+
+      return true;
+    },
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email!;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.username = existingUser.username;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
 } satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
--- src/server/db.ts
+++ src/server/db.ts
@@ -17,35 +17,9 @@ const adapter = new PrismaLibSQL(libsql);
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
-
-// import { PrismaClient } from "@prisma/client";
-// import { PrismaLibSQL } from "@prisma/adapter-libsql";
-// import { createClient } from "@libsql/client";
-// import { env } from "@/env.mjs";
-
-// const libsql = createClient({
-//   url: env.TURSO_DATABASE_URL,
-//   authToken: env.TURSO_AUTH_TOKEN,
-// });
-
-// const adapter = new PrismaLibSQL(libsql);
-
-// const createPrismaClient = () =>
-//   new PrismaClient({
-//     adapter,
-//     log:
-//       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-//   });
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: ReturnType<typeof createPrismaClient> | undefined;
-// };
-
-// export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-// if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
