49337df004d568a77f39c4a267178b4ee5bb6781
pheralb <pablohdez4340@gmail.com>
2024-03-21 11:57:29 +0000

    Add MDX support and configure options

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,21 @@
 await import("./src/env.mjs");
+import withMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   experimental: {
     optimizePackageImports: ["lucide-react"],
+    mdxRs: true,
   },
 };
 
-export default nextConfig;
+const MDXOptions = withMDX({
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+export default MDXOptions(nextConfig);
