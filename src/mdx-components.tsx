bdb6c6e26de8e6e88dd8ac8853eea4ae4ca8ad01
pheralb <pablohdez4340@gmail.com>
2024-03-23 15:03:06 +0000

    Add Bright component to MDXComponents

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,5 +1,6 @@
 import type { MDXComponents } from "mdx/types";
 
+import { Code } from "bright";
 import {
   TypographyH1,
   TypographyH2,
@@ -7,12 +8,20 @@ import {
   TypographyH4,
 } from "@/ui/typography";
 
+
+Code.theme = {
+  dark: "github-dark",
+  light: "github-light",
+  lightSelector: "html.light",
+};
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => <TypographyH1>{children}</TypographyH1>,
     h2: ({ children }) => <TypographyH2>{children}</TypographyH2>,
     h3: ({ children }) => <TypographyH3>{children}</TypographyH3>,
     h4: ({ children }) => <TypographyH4>{children}</TypographyH4>,
+    pre: Code,
     ...components,
   };
 }
