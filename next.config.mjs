950e7eece85d6ff5bdcbf38a6eb9fcf1e7dc13bb
pheralb <pablohdez4340@gmail.com>
2024-03-23 15:02:31 +0000

    Remove rehypePrettyCode plugin from MDX options

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 await import("./src/env.mjs");
 import withMDX from "@next/mdx";
 
-// MDX Plugins:
-import rehypePrettyCode from "rehype-pretty-code";
-
-/** @type {import('rehype-pretty-code').Options} */
-const rehypePrettyOptions = {
-  keepBackground: false,
-  theme: "vitesse-dark",
-};
-
 // Next.js config:
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -24,7 +15,8 @@ const nextConfig = {
 // MDX Options:
 const MDXOptions = withMDX({
   options: {
-    rehypePlugins: [[rehypePrettyCode, rehypePrettyOptions]],
+    remarkPlugins: [],
+    rehypePlugins: [],
   },
 });
 
