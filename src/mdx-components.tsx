e425805010894c49fc87eab20bc5997ff1d43c18
pheralb <pablohdez4340@gmail.com>
2024-03-24 11:48:43 +0000

    Update MDX components and add new styles for ``ul``

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -6,12 +6,14 @@ import {
   TypographyH2,
   TypographyH3,
   TypographyH4,
+  TypographyListUl,
+  TypographyP,
 } from "@/ui/typography";
-
+import Link from "next/link";
 
 Code.theme = {
-  dark: "github-dark",
-  light: "github-light",
+  dark: "poimandres",
+  light: "nord",
   lightSelector: "html.light",
 };
 
@@ -19,9 +21,23 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => <TypographyH1>{children}</TypographyH1>,
     h2: ({ children }) => <TypographyH2>{children}</TypographyH2>,
-    h3: ({ children }) => <TypographyH3>{children}</TypographyH3>,
+    h3: ({ children }) => (
+      <TypographyH3 className="text-2xl lg:text-3xl [&(:first-child)]:mb-5 [&:not(:first-child)]:mt-10">
+        {children}
+      </TypographyH3>
+    ),
     h4: ({ children }) => <TypographyH4>{children}</TypographyH4>,
+    p: ({ children }) => <TypographyP>{children}</TypographyP>,
+    a: ({ children, href }) => (
+      <Link
+        href={href!}
+        className="underline decoration-neutral-800 decoration-dotted underline-offset-4 hover:opacity-80 dark:decoration-neutral-400"
+      >
+        {children}
+      </Link>
+    ),
     pre: Code,
+    ul: ({ children }) => <TypographyListUl>{children}</TypographyListUl>,
     ...components,
   };
 }
