c487cfdd001ecf3dba63d75019e5812cec95be45
pheralb <pablohdez4340@gmail.com>
2024-03-24 20:35:27 +0000

    Refactor GET function in route.ts to include transaction and update analytics

--- src/app/api/url/route.ts
+++ src/app/api/url/route.ts
@@ -15,14 +15,38 @@ export const GET = async (req: NextRequest) => {
     );
   }
 
-  // Get data from query:
-  const data = (await db.links.findFirst({
-    where: {
-      slug: {
-        equals: params,
+  const data = await db.$transaction(async (tx) => {
+    // 1. Check if the slug exists:
+    const slugExists = await tx.links.findFirst({
+      where: {
+        slug: params,
       },
-    },
-  }))!;
+    });
+
+    // 2. If not found, return null:
+    if (!slugExists) {
+      return null;
+    }
+
+    // 3. Update analytics:
+    const browser = req.headers.get("user-agent");
+    const ipAddress = req.headers.get("x-real-ip");
+
+
+    await tx.links.update({
+      where: {
+        slug: params,
+      },
+      data: {
+        clicks: {
+          increment: 1,
+        },
+        lastClicked: new Date(),
+      },
+    });
+
+    return slugExists;
+  });
 
   // Return (/) if not found (404):
   if (!data) {
