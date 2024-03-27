9665d1a2e815c08ec1ddf55431b6f7e7141d72fb
pheralb <pablohdez4340@gmail.com>
2024-03-27 16:44:59 +0000

    Refactor Alert component to support custom styling and icon size

--- src/ui/alert.tsx
+++ src/ui/alert.tsx
@@ -6,24 +6,38 @@ import { AlertCircle, CheckCircle, InfoIcon, XCircleIcon } from "lucide-react";
 interface AlertProps {
   variant: "error" | "success" | "warning" | "info";
   children: ReactNode;
+  className?: string;
+  containerClassName?: string;
+  iconSize?: number;
 }
 
 const Alert = (props: AlertProps) => {
   return (
     <div
       className={cn(
-        "flex w-full items-center space-x-2 p-3 text-sm rounded-md",
-        props.variant === "error" && "bg-red-50 text-red-700",
-        props.variant === "success" && "bg-green-50 text-green-700",
-        props.variant === "warning" && "bg-yellow-50 text-yellow-700",
-        props.variant === "info" && "bg-blue-50 text-blue-700",
+        "w-full rounded-md p-3 text-sm",
+        props.variant === "error" && "dark:text-red-200 dark:bg-red-900/20",
+        props.variant === "success" && "dark:text-green-200 dark:bg-green-900/20",
+        props.variant === "warning" && "dark:text-yellow-200 dark:bg-yellow-900/20",
+        props.variant === "info" && "text-blue-900 dark:text-blue-200 bg-blue-700/20 dark:bg-blue-900/20",
+        props.className,
       )}
     >
-      {props.variant === "error" && <XCircleIcon size={20} />}
-      {props.variant === "success" && <CheckCircle size={20} />}
-      {props.variant === "warning" && <AlertCircle size={20} />}
-      {props.variant === "info" && <InfoIcon size={20} />}
-      <span>{props.children}</span>
+      <div
+        className={cn("flex items-center space-x-2", props.containerClassName)}
+      >
+        {props.variant === "error" && (
+          <XCircleIcon size={20 || props.iconSize} />
+        )}
+        {props.variant === "success" && (
+          <CheckCircle size={20 || props.iconSize} />
+        )}
+        {props.variant === "warning" && (
+          <AlertCircle size={20 || props.iconSize} />
+        )}
+        {props.variant === "info" && <InfoIcon size={20 || props.iconSize} />}
+        <span>{props.children}</span>
+      </div>
     </div>
   );
 };
