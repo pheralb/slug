25aeeb05f86243057c305eddde22d578dd04fb4d
pheralb <pablohdez4340@gmail.com>
2024-03-29 19:42:27 +0000

    Remove unused dependencies and files

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,13 @@
 await import("./src/env.mjs");
-import withMDX from "@next/mdx";
 
 // Next.js config:
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  pageExtensions: ["ts", "tsx", "js", "jsx"],
   experimental: {
     optimizePackageImports: ["lucide-react"],
-    mdxRs: true,
   },
 };
 
-// MDX Options:
-const MDXOptions = withMDX({
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-
-export default MDXOptions(nextConfig);
+export default nextConfig;
