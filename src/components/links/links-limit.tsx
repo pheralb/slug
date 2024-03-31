2ecde616c84ab5b9ac640f15b2831c2c5f79f627
pheralb <pablohdez4340@gmail.com>
2024-03-31 22:08:28 +0100

    Update delete-link and links-limit components

--- src/components/links/links-limit.tsx
+++ src/components/links/links-limit.tsx
@@ -1,3 +1,4 @@
+import { buttonVariants } from "@/ui/button";
 import {
   Tooltip,
   TooltipContent,
@@ -5,7 +6,7 @@ import {
   TooltipTrigger,
 } from "@/ui/tooltip";
 import { cn } from "@/utils";
-import { PackageIcon, TriangleAlertIcon } from "lucide-react";
+import { CircleDashedIcon, TriangleAlertIcon } from "lucide-react";
 
 interface LinksLimitProps {
   userLinks: number;
@@ -19,7 +20,12 @@ const LinksLimit = ({ userLinks, maxLinks }: LinksLimitProps) => {
     <TooltipProvider delayDuration={500}>
       <Tooltip>
         <TooltipTrigger asChild>
-          <div className="flex cursor-default items-center justify-end font-mono text-sm">
+          <div
+            className={buttonVariants({
+              variant: "outline",
+              className: "cursor-default font-mono shadow-none",
+            })}
+          >
             <div
               className={cn(
                 mid ? "text-yellow-500" : "",
@@ -30,13 +36,12 @@ const LinksLimit = ({ userLinks, maxLinks }: LinksLimitProps) => {
               {max ? (
                 <TriangleAlertIcon size={14} />
               ) : (
-                <PackageIcon size={14} />
+                <CircleDashedIcon size={14} />
               )}
               <span>
                 {userLinks < 10 ? `0${userLinks}` : userLinks}
                 {"/"}
                 {maxLinks < 10 ? `0${maxLinks}` : maxLinks}
-                {" links "}
               </span>
             </div>
           </div>
